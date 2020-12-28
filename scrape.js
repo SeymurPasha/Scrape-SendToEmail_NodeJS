fd8641f4e4669b94afe704d946781423cfbb458c
SeymurPasha <seymurpashayev2018@gmail.com>
2020-12-28 16:46:59 +0400

    test

--- scrape.js
+++ scrape.js
@@ -7,7 +7,7 @@ require('dotenv').config();
 var transport = require('nodemailer-smtp-transport');
 
 var schedule = require('node-schedule');
-var j = schedule.scheduleJob('40 16 * * *', function(){
+var j = schedule.scheduleJob('55 16 * * *', function(){
   
   request('https://www.qafqazislam.com/index.php?lang=az&sectionid=123', (error,response, html) => {
   if(!error && response.statusCode == 200) {
