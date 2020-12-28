cd8b5486609a039569d51049a714281bfdf3b44f
SeymurPasha <seymurpashayev2018@gmail.com>
2020-12-28 10:55:34 +0400

    first issue

---
+++ scrape.js
@@ -0,0 +1,54 @@
+const request = require('request')
+const cheerio = require('cheerio')
+const fs = require('fs');
+const nodemailer = require('nodemailer');
+const download = require('image-downloader')
+
+
+
+request('https://www.qafqazislam.com/index.php?lang=az&sectionid=123', (error,response, html) => {
+if(!error && response.statusCode == 200) {
+    const $ = cheerio.load(html)
+
+    const linkToImage = 'https://www.qafqazislam.com' +  $('.containertext2').children('img').attr('src') 
+    console.log(linkToImage);
+
+    const options = {
+      url: linkToImage,
+      dest: './images'                
+    }
+    
+    download.image(options)
+      .then(({ filename }) => {
+        console.log('Saved to', filename)  
+      })
+      .catch((err) => console.error(err))
+
+
+    setTimeout(() => {
+      sendToMe()
+    }, 10000); 
+    
+   
+      
+}
+})
+
+  const sendToMe =  () => {
+  var smtpTransport = nodemailer.createTransport({ service: 'Gmail', port: 465, auth: { user: 'pashayevseymur42@gmail.com', pass: 'Pashayev1991@@' } }); 
+  var mailOptions = { from: 'pashayevseymur42@gmail.com', to: 'seymurpashayev2018@gmail.com', subject: 'Namaz vaxtı', attachments:[{path:'./images/12.jpg'}] }; 
+  smtpTransport.sendMail(mailOptions, (error, response) => { 
+    
+  if(error) { res.send(error) 
+  }
+  
+  else { console.log('Success') 
+  } 
+  smtpTransport.close(); 
+  
+  }); 
+}
+
+
+
+
