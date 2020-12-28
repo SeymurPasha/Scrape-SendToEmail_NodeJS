bc69ac46057c8ff03aef0d8d7cfd36f9601b7af5
SeymurPasha <seymurpashayev2018@gmail.com>
2020-12-28 15:03:56 +0400

    small fix

--- scrape.js
+++ scrape.js
@@ -4,12 +4,13 @@ const fs = require('fs');
 const nodemailer = require('nodemailer');
 const download = require('image-downloader')
 require('dotenv').config();
+var transport = require('nodemailer-smtp-transport');
 
 
 
 
   request('https://www.qafqazislam.com/index.php?lang=az&sectionid=123', (error,response, html) => {
-if(!error && response.statusCode == 200) {
+  if(!error && response.statusCode == 200) {
     const $ = cheerio.load(html)
 
     const linkToImage = 'https://www.qafqazislam.com' +  $('.containertext2').children('img').attr('src') 
@@ -36,7 +37,7 @@ if(!error && response.statusCode == 200) {
 
 
   const sendToMe =  () => {
-  var smtpTransport = nodemailer.createTransport({ service: 'Gmail', port: 465, auth: { user: 'pashayevseymur42@gmail.com', pass: process.env.password } }); 
+  var smtpTransport = nodemailer.createTransport(transport({ service: 'Gmail', port: 465, auth: { user: 'pashayevseymur42@gmail.com', pass: process.env.password } })); 
   var mailOptions = { from: 'pashayevseymur42@gmail.com', to: 'seymurpashayev2018@gmail.com', subject: 'Namaz vaxtı', attachments:[{path:'./images/12.jpg'}] }; 
   smtpTransport.sendMail(mailOptions, (error, response) => { 
     
